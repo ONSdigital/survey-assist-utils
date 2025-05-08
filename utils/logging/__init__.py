3fb6d9fa840d52c95d630e04d1eef648e9590802
dl-ons <dominic.love@ons.gov.uk>
2025-05-08 17:50:41 +0100

    fix lint errors

--- utils/logging/__init__.py
+++ utils/logging/__init__.py
@@ -1,5 +1,4 @@
-"""
-Survey Assist Utils Logging Package
+"""Survey Assist Utils Logging Package.
 
 This package provides a unified logging interface for Survey Assist applications,
 supporting both local development and GCP environments.
@@ -7,4 +6,4 @@ supporting both local development and GCP environments.
 
 from .logging_utils import get_logger
 
-__all__ = ['get_logger'] 
+__all__ = ["get_logger"]
